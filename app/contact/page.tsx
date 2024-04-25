3dbcd33ac8184315e6ec77f34d246bc9e4e7a06a
prajjwal <howtoshout@gmail.com>
2024-04-26 00:32:10 +0530

    contact scaffold

---
+++ app/contact/page.tsx
@@ -0,0 +1,33 @@
+import PageHeader from "@/components/PageHeader";
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <>
+      <PageHeader title="Contact Us " />
+      <section className=" bg-[#F3F4F6]">
+        <div className="max-w-7xl m-auto  py-20">
+          <div className="flex  items-center">
+            <div className="flex flex-col gap-10">
+              <span className="text-6xl font-extrabold">
+                Feel free to keep in touch
+              </span>
+              <span>
+                Vulputate egestas nullam volutpat diam nisi at venenatis
+                adipiscing massa posuere massa nulla massa id integer.
+              </span>
+              <span>
+                Cras ullamcorper fermentum arcu in sed fermentum velit nulla
+                scelerisque pharetra tristique lectus justo faucibus purus est
+                purus gravida nibh odio ante.
+              </span>
+            </div>
+            <div>
+              <form action=""></form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
