73bfaaad67154da2ed4b245abd17187c2acf17ff
prajjwal <howtoshout@gmail.com>
2024-05-06 19:43:47 +0530

    done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -22,39 +22,27 @@ export default function AboutUs() {
               <span className="text-secondary font-bold text-2xl">
                 Hi there!
               </span>
-              <span className="text-primary font-extrabold text-4xl md:text-6xl">
-                We are best of Advertisers
+              <span className="text-primary font-extrabold text-base md:text-xl">
+                At AppExcel, we are driven by passion, innovation and
+                entrepreneurial commitment to help you excell in your marketing
+                & user acquition journey
               </span>
             </div>
           </div>
         </div>
         <div className="max-w-7xl m-auto text-lg">
           <div className=" flex md:flex-row flex-col justify-center md:items-start items-center mb-32">
-            <span className="font-bold text-lg w-1/4">Our story</span>
+            <span className="font-bold text-lg w-1/4">Vision</span>
             <div className="w-3/4">
               <div>
                 <p>
-                  With its inception in 2022, GMA is a powerful team of super
-                  energized folks with more than a decade&apos;s industry experience,
-                  driven to generate substantial business for our partners.
-                </p>
-                <p>
-                  With passion, foresight, and a customer centric focus, we aim
-                  to navigate our journey to transform the way consumers engage
-                  with their phones & apps, with the sole objective being the
-                  last mile conversions.
-                </p>
-                <p>
-                  Ever since inception, GMA has been helping mobile businesses
-                  to evolve by achieving their marketing goals & fabricating
-                  their expectations.
-                </p>
-                <p>
-                  We&apos;d be glad to help you scale your business & become a part
-                  of your growth journey.
+                  AppExcel&apos;s vision is to redefine digital advertising
+                  through innovation and strategic thinking. Our visionary
+                  foundation propels us forward, shaping the future of
+                  advertising technology.
                 </p>
               </div>
-              <div className="grid grid-cols-3 gap-5 items-center justify-center">
+              {/* <div className="grid grid-cols-3 gap-5 items-center justify-center">
                 <Image
                   className="col-span-2 w-full"
                   src="/about/1.jpg"
@@ -83,65 +71,68 @@ export default function AboutUs() {
                   width={500}
                   height={500}
                 />
-              </div>
+              </div> */}
             </div>
           </div>
-          <div className=" flex md:flex-row flex-col  md:items-start items-center  mb-32">
-            <span className="font-bold text-lg w-1/4">Our Vision</span>
+          <div className=" flex md:flex-row flex-col  md:items-start items-center  mb-32 mr-32">
+            <span className="font-bold text-lg w-1/4">
+              Champions striving for excellence
+            </span>
             <div className="w-3/4 border-t-2 border-cyanno pt-10">
               <div>
                 <p>
-                  With its inception in 2022, GMA is a powerful team of super
-                  energized folks with more than a decade&apos;s industry experience,
-                  driven to generate substantial business for our partners.
-                </p>
-                <p>
-                  With passion, foresight, and a customer centric focus, we aim
-                  to navigate our journey to transform the way consumers engage
-                  with their phones & apps, with the sole objective being the
-                  last mile conversions.
-                </p>
-                <p>
-                  Ever since inception, GMA has been helping mobile businesses
-                  to evolve by achieving their marketing goals & fabricating
-                  their expectations.
-                </p>
-                <p>
-                  We&apos;d be glad to help you scale your business & become a part
-                  of your growth journey.
+                  We are professionals driven by expertise and a passion for
+                  excelling. Our diverse skill set fuels our ability to deliver
+                  unparalleled solutions, navigating the dynamic landscape of
+                  digital advertising with precision.
                 </p>
               </div>
             </div>
           </div>
           <div className=" flex  md:flex-row flex-col md:items-start items-center pb-20">
-            <span className="font-bold text-lg w-1/4">Our Mission</span>
+            <span className="font-bold text-lg w-1/4">
+              Top priority - our parnters
+            </span>
             <div className="w-3/4 border-t-2 border-cyanno pt-10">
               <div>
                 <p>
-                  With its inception in 2022, GMA is a powerful team of super
-                  energized folks with more than a decade&apos;s industry experience,
-                  driven to generate substantial business for our partners.
+                  At AppExcel, we our priority is our you. We forge lasting
+                  partnerships, tailor our services to your unique needs, and
+                  are committed to providing a seamless experience that exceeds
+                  expectations. Your goals are our goals.
                 </p>
+              </div>
+            </div>
+          </div>
+
+          <div className=" flex  md:flex-row flex-col md:items-start items-center pb-20">
+            <span className="font-bold text-lg w-1/4">Innovation</span>
+            <div className="w-3/4 border-t-2 border-cyanno pt-10">
+              <div>
                 <p>
-                  With passion, foresight, and a customer centric focus, we aim
-                  to navigate our journey to transform the way consumers engage
-                  with their phones & apps, with the sole objective being the
-                  last mile conversions.
+                  Commitment to visionary innovation. Our team is driven by a
+                  shared passion for redefining digital advertising through
+                  forward-thinking strategies and cutting-edge solutions
                 </p>
+              </div>
+            </div>
+          </div>
+          <div className=" flex  md:flex-row flex-col md:items-start items-center pb-20">
+            <span className="font-bold text-lg w-1/4">
+              Data-Driven Precision
+            </span>
+            <div className="w-3/4 border-t-2 border-cyanno pt-10">
+              <div>
                 <p>
-                  Ever since inception, GMA has been helping mobile businesses
-                  to evolve by achieving their marketing goals & fabricating
-                  their expectations.
-                </p>
-                <p>
-                  We&apos;d be glad to help you scale your business & become a part
-                  of your growth journey.
+                  Campaigns succeed from data. Our experts leverage advanced
+                  algorithms and data analytics to ensure precision in
+                  targeting, providing insights that drive informed
+                  decision-making
                 </p>
               </div>
             </div>
           </div>
         </div>
-        
       </section>
     </>
   );
